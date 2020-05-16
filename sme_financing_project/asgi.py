f08f320b8a5c35c094dd337b9548fa4bed61b6db
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2020-05-17 00:03:24 +0100

    fix: Make styling changes

--- sme_financing_project/asgi.py
+++ sme_financing_project/asgi.py
@@ -1,4 +1,5 @@
-"""ASGI config for sme_financing_project project.
+"""
+ASGI config for sme_financing_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- sme_financing_project/wsgi.py
+++ sme_financing_project/wsgi.py
@@ -1,4 +1,5 @@
-"""WSGI config for sme_financing_project project.
+"""
+WSGI config for sme_financing_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
