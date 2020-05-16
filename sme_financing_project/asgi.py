3f713344e5c7efb592ecfd73857f703fdd4ef28c
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2020-05-16 23:57:59 +0100

    fix: Make styling changes

--- sme_financing_project/asgi.py
+++ sme_financing_project/asgi.py
@@ -1,11 +1,9 @@
-"""
-ASGI config for sme_financing_project project.
+"""ASGI config for sme_financing_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/"""
 
 import os
 
--- sme_financing_project/wsgi.py
+++ sme_financing_project/wsgi.py
@@ -1,11 +1,9 @@
-"""
-WSGI config for sme_financing_project project.
+"""WSGI config for sme_financing_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/"""
 
 import os
 
