b3d505894ef7041e025c57e82fa4b62a524f4bf7
Nafiu Adegbite <youngguru23@gmail.com>
2020-05-18 22:14:52 +0100

    .

--- sme_financing_app_one/serializers.py
+++ sme_financing_app_one/serializers.py
@@ -3,8 +3,10 @@ from sme_financing_app_one import models
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    """Create User Profile Serializer."""
+    """Create User Profile Serializer.""" 
+
     class Meta:
+        
         """Create user."""
 
         model = models.UserProfile
@@ -17,6 +19,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             }
         }
 
+    @classmethod
     def create(self, validated_data):
         """Create and return a new user."""
         user = models.UserProfile.objects.create_user(
