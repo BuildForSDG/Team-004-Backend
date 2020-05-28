b2a1a1894623fe97fdebcf20703b4b4930cdae96
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2020-05-28 07:17:00 +0100

    fix: Remove unused variable

--- user_management/views.py
+++ user_management/views.py
@@ -26,7 +26,7 @@ class AuthenticateUserView(ObtainAuthToken):
         )
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        token = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'user_id': user.pk,
